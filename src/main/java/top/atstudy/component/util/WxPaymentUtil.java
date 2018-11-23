0f12f913e9f2f9dee6c8e8149d0bfa42144dff5c
huangdexin <735513870@qq.com>
2018-11-23 10:45:35 +0800

    util init

---
+++ src/main/java/top/atstudy/component/util/WxPaymentUtil.java
@@ -0,0 +1,117 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+package top.atstudy.component.util;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.ConnectException;
+import java.net.URL;
+import java.security.SecureRandom;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+import javax.net.ssl.*;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import top.atstudy.component.enums.EnumRequestMethod;
+
+public class WxPaymentUtil {
+    private static final Logger log = LoggerFactory.getLogger(WxPaymentUtil.class);
+
+    public WxPaymentUtil() {
+    }
+
+    private static String httpRequest(String requestUrl, EnumRequestMethod method, String outputStr) {
+        StringBuffer buffer = new StringBuffer();
+
+        try {
+            TrustManager[] tm = new TrustManager[]{new MyX509TrustManager()};
+            SSLContext sslContext = SSLContext.getInstance("SSL", "SunJSSE");
+            sslContext.init((KeyManager[])null, tm, new SecureRandom());
+            SSLSocketFactory ssf = sslContext.getSocketFactory();
+            URL url = new URL(requestUrl);
+            HttpsURLConnection httpUrlConn = (HttpsURLConnection)url.openConnection();
+            httpUrlConn.setSSLSocketFactory(ssf);
+            httpUrlConn.setDoOutput(true);
+            httpUrlConn.setDoInput(true);
+            httpUrlConn.setUseCaches(false);
+            httpUrlConn.setRequestMethod(method.getCode());
+            if (EnumRequestMethod.GET == method) {
+                httpUrlConn.connect();
+            }
+
+            if (null != outputStr) {
+                OutputStream outputStream = httpUrlConn.getOutputStream();
+                outputStream.write(outputStr.getBytes("UTF-8"));
+                outputStream.close();
+            }
+
+            InputStream inputStream = httpUrlConn.getInputStream();
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "utf-8");
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            String str = null;
+
+            while((str = bufferedReader.readLine()) != null) {
+                buffer.append(str);
+            }
+
+            bufferedReader.close();
+            inputStreamReader.close();
+            inputStream.close();
+            httpUrlConn.disconnect();
+            log.info(" ===>> https request success ... ");
+            log.info(" ===>> {}", buffer.toString());
+        } catch (ConnectException var13) {
+            log.error(" ===>> https request connection timed out. error: {}", var13);
+        } catch (Exception var14) {
+            log.error(" ===>> https request error:{}", var14);
+        }
+
+        return buffer.toString();
+    }
+
+    public static String get(String url, Map<String, Object> params) {
+        if (params != null) {
+            StringBuilder stringBuilder = new StringBuilder("");
+            Iterator var3 = params.entrySet().iterator();
+
+            while(var3.hasNext()) {
+                Entry<String, Object> entry = (Entry)var3.next();
+                stringBuilder.append("&").append((String)entry.getKey()).append("=").append(entry.getValue());
+            }
+
+            String paramsStr = stringBuilder.toString();
+            url = url + "?" + paramsStr.substring(1, paramsStr.length());
+        }
+
+        log.info(" ===>> request: {}", url);
+        return httpRequest(url, EnumRequestMethod.GET, (String)null);
+    }
+
+    public static String post(String url, String params) {
+        return httpRequest(url, EnumRequestMethod.POST, params);
+    }
+
+    static class MyX509TrustManager implements X509TrustManager {
+        public MyX509TrustManager() {
+        }
+
+        public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+        }
+
+        public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+        }
+
+        public X509Certificate[] getAcceptedIssuers() {
+            return null;
+        }
+    }
+}
---
+++ src/main/java/top/atstudy/component/util/WxUploadUtil.java
@@ -0,0 +1,111 @@
+package top.atstudy.component.util;
+
+import com.alibaba.fastjson.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Formatter;
+
+public class WxUploadUtil {
+    private static final Logger log = LoggerFactory.getLogger(WxUploadUtil.class);
+
+    public WxUploadUtil() {
+    }
+
+    public static String httpRequest(String requestUrl, byte[] data, String fileName) {
+        StringBuffer buffer = new StringBuffer();
+
+        try {
+            URL url = new URL(requestUrl);
+            HttpURLConnection httpUrlConn = (HttpURLConnection)url.openConnection();
+            httpUrlConn.setDoInput(true);
+            httpUrlConn.setDoOutput(true);
+            httpUrlConn.setUseCaches(false);
+            httpUrlConn.setRequestProperty("Connection", "Keep-Alive");
+            httpUrlConn.setRequestProperty("Charset", "UTF-8");
+            String BOUNDARY = "----------" + System.currentTimeMillis();
+            httpUrlConn.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + BOUNDARY);
+            StringBuilder sb = new StringBuilder();
+            sb.append("--");
+            sb.append(BOUNDARY);
+            sb.append("\r\n");
+            sb.append("Content-Disposition: form-data;name=\"media\";filelength=\"" + data.length + "\";filename=\"" + fileName + ".xlsx\"\r\n");
+            sb.append("Content-Type:application/octet-stream\r\n\r\n");
+            byte[] head = sb.toString().getBytes("utf-8");
+            OutputStream outputStream = new DataOutputStream(httpUrlConn.getOutputStream());
+            outputStream.write(head);
+            outputStream.write(data);
+            byte[] foot = ("\r\n--" + BOUNDARY + "--\r\n").getBytes("utf-8");
+            outputStream.write(foot);
+            outputStream.flush();
+            outputStream.close();
+            InputStream inputStream = httpUrlConn.getInputStream();
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "utf-8");
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            String str = null;
+
+            while((str = bufferedReader.readLine()) != null) {
+                buffer.append(str);
+            }
+
+            bufferedReader.close();
+            inputStreamReader.close();
+            inputStream.close();
+            httpUrlConn.disconnect();
+        } catch (IOException var15) {
+            System.out.println("发送POST请求出现异常！" + var15);
+            var15.printStackTrace();
+        }
+
+        return buffer.toString();
+    }
+
+    private static String byteToHex(byte[] hash) {
+        Formatter formatter = new Formatter();
+        byte[] var2 = hash;
+        int var3 = hash.length;
+
+        for(int var4 = 0; var4 < var3; ++var4) {
+            byte b = var2[var4];
+            formatter.format("%02x", b);
+        }
+
+        String result = formatter.toString();
+        formatter.close();
+        return result;
+    }
+
+    public static void main(String[] args) {
+        try {
+            String url = "https://qyapi.weixin.qq.com/cgi-bin/media/upload?access_token=ACCESS_TOKEN&type=TYPE";
+            String filePath = "E://temp_poi/test.xls";
+            File file = new File(filePath);
+            FileInputStream fis = new FileInputStream(file);
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            byte[] b = new byte[10240];
+            boolean var7 = false;
+
+            int len;
+            while((len = fis.read(b)) != -1) {
+                bos.write(b, 0, len);
+            }
+
+            byte[] data = bos.toByteArray();
+            String accessToken = "cG-4CICX1pMQ0qLWCP74eUF4Vz-BK721QcUl9dkwBdDinoLzFQPaXAsL7veztjaN94TusIiqFHGEcOQyamjZKPZGtrWyVxAzdEcjMMzG0yotFvengJPPJ0f_HN56MVAD1Ei0IWeaZ6_BhVgbg7W5PWPqMYkyCeoSQKIvT6Jv3nP40QmQihTDZQKMfarD1fJ7DVP-eotwHeAGsVI4nJ53-Q";
+            String type = "file";
+            String reqUrl = url.replace("ACCESS_TOKEN", accessToken).replace("TYPE", type);
+            String response = httpRequest(reqUrl, data, "临时文件名.xlxs");
+            log.info("==>> response jsonObject: {}", response);
+            JSONObject jsonObject = JSONObject.parseObject(response);
+            if (jsonObject != null && jsonObject.getInteger("errcode") != 0) {
+                ;
+            }
+        } catch (Exception var14) {
+            var14.printStackTrace();
+        }
+
+    }
+}
